aaf33f015081c4be28c5f5601145b8a29ac87a80
thsghkfkd12 <thsghkfkd12@gmail.com>
2024-12-05 21:45:53 +0900

    2024-12-05자바수업 내용입니다

--- 문제.java
+++ 문제.java
@@ -1,56 +1,244 @@
-package java_study_1126;
+package java_study_1205;
 
 import java.util.Random;
 
 public class 문제 {
 
 	public static void main(String[] args) {
+		/*
+		 * [문제1]문항
+			○○은 final 키워드를 사용합니다. 실행 중 값 변경이 불가능한 게 특징입니다.
+			 ○○은 무엇일까요?
+			1. 변수
+			2. 실수
+			3. 상수
+			4. 정수
+		 */
+		// 1번 정답 상수
 		
-//		1번)
-//    10개의 정수를 저장하는 배열을 선언하고 1부터 10까지의 값으로 초기화한 후,
-//    2번째 인덱스와 5번째 인덱스를 곱한 값을 출력하시오.
-		int []sum = new int[10];
-		Random random = new Random();
+		/*
+		 * [문제2 문항
+ 		문자형변수 x를 선언하고 “10”을 할당 후
+		정수형 변수 y를 선언하고 x의 값을 y에 대입하세요. *문자에서 정수형으로 ‘형변환’ 해야합니다.
+		 */
+		String a = "10"; 
+		int y = Integer.parseInt(a);
+		System.out.println(y);
 		
-		for(int i=0; i<sum.length; i++) {
-			sum[i] = random.nextInt(100)+1;
-			
+		/*
+		 * [문제3]문항
+			다음 중 잘못된 식별자(변수이름)는?
+			1. monster3
+			2. apple_13_pro
+			3. 3apple
+			4. AaaAAA
+		 */ 
+		// 3번정답 3apple
+		
+		/*
+		 * [문제4] 문항 1부터 100까지의 숫자 중 짝수인지 홀수인지 판별하는 프로그램을 작성해주세요.
+		 */
+		
+		for(int i=1; i<=100; i++) {
+			if(i % 2 == 0) {
+				System.out.println("짝수 : " +i);
+			}else {
+				System.out.println("홀수 : " +i);
+			}
 		}
-		System.out.println(sum[2]+"*"+sum[5]+"="+sum[2]*sum[5]);
 		
-//    2번)
-//    1번 문항 이어서
-//    배열에 저장된 값을 모두 더하는 프로그램을 작성하세요.
+		/*
+		 * [문제5]문항
+		boolean 면허증소지 = true;
+		boolean 운전가능 = !면허증소지;
+		최종 운전가능 값은 무엇일까요? 
+		 */ //false
+		boolean 면허증소지 = true;
+		boolean 운전가능 = !면허증소지;
+		System.out.println(운전가능);
+		
+		/*
+		 * [문제6]문항 1부터 100까지의 숫자 중 3의 배수의 합을 구하는 프로그램을 작성하세요.
+		 */
 		int s = 0;
-		for(int i=0; i<sum.length; i++) {
-			s += sum[i];
-		}
-		System.out.println("총합 : " +s);
-   
-//    3번)
-//    크기가 10인 정수 배열을 선언하고 랜덤한 값(1~100)으로 초기화한 후, 배
-//    열의 요소들 중 짝수만 출력하는 프로그램을 작성하세요.
-		for(int i=0; i<sum.length; i++) {
-			sum[i] = random.nextInt(100)+1;
-			int d = sum[i];
-			if(d % 2 == 0) {
-				System.out.println("짝수 : "+d);
+		for(int i=1; i<=100; i++) {
+			if(i % 3 == 0) {
+				System.out.println(i);
 			}
 		}
 		
-
-//    4번)
-//    크기가 10인 정수 배열을 선언하고 랜덤한 값(1~100)으로 초기화한 후,
-//    최댓값을 구하는 프로그램을 작성하세요.
-		for(int i=0; i<sum.length; i++) {
-			sum[i] = random.nextInt(100)+1;
-			int a = sum[i];
-			if(a != 100) {
-				System.out.println("최대 값 : " +a);
+		/*
+		 * [문제7]문항
+			길동이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때,
+			모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 출력하세요.
+    		int n = 15; //사람의 수 예) 15명이 최소 한 조각씩 먹기 위해서 최소 3판이 필요합니다.
+ 			n이 15라고 가정하면 피자는 3판이 필요합니다.
+		 */
+		
+		int n = 15;
+		int sizze = 3;
+		int sizzes= 0;
+		sizzes = n / sizze;
+		System.out.println("피자수 : "+sizzes);
+		
+		/*
+		 * [문제8]문항
+		 *  사용자로부터 정수를 입력받아 팩토리얼을 계산하는 프로그램을 작성하세요.
+		 *   (예: 5! = 5 * 4 * 3 * 2 * 1)
+		 */
+		int num = 5;
+		int f = 1;
+		for(int i=1; i<num; i++) {
+			f = f * i;
+			System.out.println(f);
+		}
+		
+		/*
+		 * [문제9]문항 2부터 5까지의 구구단을 출력하는 프로그램을 작성하세요.
+		 */
+		for(int i=2; i<=5; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.println(i+"*"+j+"="+i*j);
 			}
 		}
-
 		
+		/*[문제10]문항
+			다음과 같은 패턴을 출력하는 프로그램을 작성하세요. 
+			****
+		 	***
+		 	**
+		 	* 
+		 */
+		for(int i=0; i<5; i++) {
+			for(int j=5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/*
+		 * [문제11]문항
+			사용자로부터 문자열을 입력받아 그 문자열의 길이를 출력하는 프로그램을 작성하세요. 
+			단, 길이가 11 이상인 문자열은 “아이디는 10글자 이하로 작성해주세요.”
+			라고 출력할 것.
+		 */
+		String userId = "홍길동";
+		if(userId.length() >= 10) { // 문자열길이가 10이상이라면?
+			System.out.println("아이디는 10글자 미만으로 작성해주세요");
+		}else  {
+			System.out.println("사용가능한 아이디입니다.");
+		}
+		
+		/*
+		 * [문제12]문항
+			열에 대한 설명 중 다른 것은?
+			① 배열은 같은 타입의 데이터들이 순차적으로 저장되는 공간이다.
+ 			② int[] array = new int[10]; 총 10개의 공간이 생성되었다.
+  			③ 배열은 항상 0부터 시작한다. 
+  			④ 반복문을 이용하여 처리하기에 적합하지 않은 자료구조다.
+		 */
+		// 12 정답 4번
+		
+		/*
+		 * [문제13]
+		문항 크기가 10인 정수 배열을 선언하고 랜덤한 값(1~100)으로 초기화한 후,
+		 배열에서 가장 큰 값을 작성하세요.
+		 */
+		
+		int[] array = {1,2,3,4,7,8,9,10}; 
+		int max = 0;
+		Random random = new Random();
+		for(int i=0; i<array.length; i++) {
+			array[i] = random.nextInt(100) +1;
+		}
+		
+		for(int i=0; i<array.length; i++) {
+			if(max < array[i]) {
+				max = array[i];
+			}
+		}
+		System.out.println("최대값 : "+max);
+		
+		/*
+		 * [문제14]문항
+		 *  크기가 10인 정수 배열을 선언하고 랜덤한 값(1~100)으로 초기화한 후, 
+		 *  배열의 모든 요소의 평균과 합산을 출력하는 프로그램을 작성하세요.
+		 */
+		
+		int[] array2 = new int[10];
+		for(int i=0; i<array2.length; i++) {
+			array2[i] = random.nextInt(100) + 1;
+			System.out.println(array2[i]);
+		}
+		
+		/*
+		 * [문제15]문항
+		 *  문자열 "Java Programming"에서 'a'의 개수를 세는 코드를 작성하세요.
+		 */
+		
+		String aa = "Java Programming";
+		String[] aa2 = aa.split("");
+		
+		int con = 0;
+		for(int i=0; i<aa2.length; i++) {
+			if(aa2[i].equals("a")) {
+				++con;
+			}
+		}
+		System.out.println(con);
+		
+		/*
+		 * [문제16]문항
+			아이스 아메리카노는 한잔에 5,500원입니다.
+ 			money가 변수로 주어질 때, 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 출력하시오.
+			int money = 15000;
+			예) 15000원이면 커피 2잔과 4000원이 남습니다.
+		 */
+		int money = 15000;
+		int aic = 5500;
+		int my = money / aic;
+		money = money % aic;
+		System.out.println("커피 : "+my);
+		System.out.println("잔돈 : "+money);
+		
+		/*
+		 * [문제17]문항
+		길동는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 
+		길동이네 반 친구들의 키가 담긴 정수 배열 array와 길동이의 키 height가
+		변수로 주어질 때, 길동이보다 키 큰 사람 수를 출력하시오.
+		int[] array = {149, 180, 192, 170};
+		int height = 167;
+		예) 149, 180, 192, 170 중 길동이보다 키가 큰 사람은
+		180, 192, 170으로 세 명입니다.
+		 */
+		int[] array3 = {149, 180, 192, 170};
+		int height = 167;
+		for(int i=0; i<array3.length; i++) {
+			if(array3[i] > height) {
+				System.out.println("길동이보다 기가큰 사람은"+array3[i]+"입니다");
+			}
+		}
+		
+		/*
+		 * [문제18 - 난이도 Up]문항군 전략가 길동이는 전쟁 중 적군이 다음과 같은
+		 * 암호 체계를 사용한다는 것을 알아냈습니다.
+		 1. 암호화된 문자열 cipher를 주고받습니다. 
+		 2. 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+		  문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 출력하시오. 
+		String cipher = "dfjardstddetckdaccccdegk";
+		int code = 4;
+		예) "dfjardstddetckdaccccdegk" 의 4번째, 8번째, 12번째, 16번째, 20번째, 24번째
+		 글자를 합친 "attack"을 출력합니다
+		 */
+		String cipher = "dfjardstddetckdaccccdegk";
+		int code = 4;
+		
+		
+		
+		
+		
+		
+
 	}
 
 }
--- 복습.java
+++ 복습.java
@@ -1,147 +1,124 @@
-package java_study_1203;
-
-import java.util.Scanner;
+package java_study_1205;
 
 public class 복습 {
 
 	public static void main(String[] args) {
 		
-		// 1. 1부터 100까지의 숫자 중 짝수만 출력하는 프로그램을 작성하세요.
-		// 1부터 100까지 범위 => for(반복문)
-		// 반복문 종류 1. for 2. while 3. do-while(본적 없음)
-		
-		for(int i=1; i<=100; i++) {
-			// % : 나머지 값 구하기
-			// == : 비교연산자
-			// if를 이용해 짝수 판단
-			// 코드가 한줄일 때 중괄호 생략가능
-			if(i % 2 == 0) System.out.println("짝수는 : "+i); 
-		}
-		
-		// 2. 1부터 10까지의 합을 구하시오
-		// 1+2+3./.......+10 == 55
-		int sum = 0;
-		for(int i=0; i<=10; i++) {
-			// +=(복합연산자) => sum = sum + i;
-			sum += i;
-		}
-		System.out.println("총합 : "+sum);
-		
-		// 3. 배열
-		String[] array = {"apple","apple","apple","banana"};
-		// 총 몇 개 ? count (증감연산자 사용 확률 높음)
-		int count = 0;
-		for(String i : array) { // for-each
-			if(i.equals("apple")) { // 자바에서 문자비교는 'equals'
-				++count; // 증감연산자
-			}
-		}
-		System.out.println(count);
-		
 		/*
-		 *  00옷가게는 10만원 이상 사면 5%, 30만원 이상 사면 10%,
-		 *  50만원 이상 사면 20% 를 할인해줍니다.
-		 *  구매한 옷의 가격 price변수에 580,000 주어질 때,
-		 *  지불해야 할 금액을 출력 하도록 코드를 완성해보세요.
+		 * 1. 사칙연산 - 거스름돈
 		 */
-		double price = 580000;
-		if(price >= 500000) {
-			price = price * 0.8; // 20% 할인
-		}else if(price >= 300000) {
-			price = price * 0.9; // 10%할인
-		}else if(price >= 100000) {
-			price = price * 0.95; // 5%할인
-		}
+		int money = 74900;
 		
-		/* [문자열 문제]
-		 * 문자열 my_string에 "hello"가 주어질 때
-		 * my_string에 들어있는 각 문자를 n만큼 반복한 문자열 출력하기
-		 * n에 3이 주어짐
-		 */
-		String my_string = "hello";
-		int n = 3;
-		// 결과 : hhheeellllllooo
-		// 힌트 : for문 -> 이중 for문
-		String result = "";
+		// 결과 -> 오만원 1장, 만원 2장, 천원 4장, 5백원1개, 100원 4개
 		
-		for(int i=0; i<my_string.length(); i++) { // 문자 길이만큼 반복
-			String word = my_string.substring(i, i+1);
-			for(int j=0; j<n; j++) { // 이중 for문
-				// n만큼 반복
-				result += word;
-			}
-		}
-		System.out.println("결과 : "+result);
-		
-		// repeat
-		String s = "Hello";
-		System.out.println(s.repeat(3));
-		
-		// repeat활용해서 위 문제 다시 풀어보기
-		String[] strArray = my_string.split("");
-		String result2 = "";
-		for(String i : strArray) {
-			result2 += i.repeat(n); // repeat 활용
-		}
-		System.out.println(result2);
-		
-		// [배열] 짝수, 홀수 개수구하기
-		/*
-		 * 정수형 배열 num_list가 주어질 때 num_list의 원소 중 짝수와 홀수의 개수를
-		 * 담은 정수형 배열 answer_list를 완성하기.
-		 */
-		
-		int[] num_list = {1,3,5,7};
-		int[] answer_list = new int[2];
-		/*
-		 * answer_list : 결과
-		 * [0,4] 즉, 0번째 인덱스엔 짝수, 1번째 인덱스엔 홀수 개수가 옴
-		 */
-		int 짝수카운트 = 0;
-		int 홀수카운트 = 0;
-		for(int i : num_list) { // for-each
-			if(i % 2 == 0) { // 짝수?
-				++짝수카운트;
-				answer_list[0] = 짝수카운트; // 0번째 인덱스(순서)에 값 대입
-			}else { // 홀수?
-				++홀수카운트;
-				answer_list[1] = 홀수카운트; // 1번째 인덱스(순서)에 값 대입
-			}
-		}
-		
-		/*
-		 * 문제 사칙연사만 사용
-		 * 상원이네 양꼬치 가게는 10인분을 먹으면 음료수
-		 * 하나를 서비스로 줍니다.
-		 * 양꼬치는 1인분에 12.000원
-		 * 음료수는 2,000원입니다.
-		 * 정수 n과 k가 변수로 주어졌을 때, 양꼬치 n인분
-		 * 과 음료수 k개를 먹었다면 
-		 * 총얼마를 지불해야 하는지 출력하시오
-		 */
-		
-		int skwe = 12000; // 양꼬치 가격
-		int drink = 2000; // 음료수 가격
-//		Scanner scan = new Scanner(System.in);
-//		System.out.print("양꼬치 개수 : ");
-//		int n1 = scan.nextInt();
-//		System.out.print("음료수 개수 : ");
-//		int k = scan.nextInt();
-		
-		int n1 = 10;
-		int k = 3;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		int 오만원 = 50000;
+        int 만원 = 10000;
+        int 천원 = 1000;
+        int 오백원 = 500;
+        int 백원 = 100;
+        int 오만원개수 = money / 오만원;
+        money = money % 오만원;
+        int 만원개수 = money / 만원;
+        money = money % 만원;
+        int 천원개수 = money / 천원;
+        money = money % 천원;
+        int 오백원개수 = money / 오백원;
+        money = money % 오백원;
+        int 백원개수 = money / 백원;
+        money = money % 백원;
+        
+        System.out.println("오만원 : "+오만원개수);
+        System.out.println("만원 : "+만원개수);
+        System.out.println("천원 : "+천원개수);
+        System.out.println("오백원 : "+오백원개수);
+        System.out.println("백원 : "+백원개수);
 
+
+		
+		/*
+		 * 그리드(탐욕) 알고리즘 ex) 거스름돈
+		 * => 선택의 순간마다 당장 눈앞에 보이는 최적의 상황만을 쫒아 최종 답에
+		 * 도달하는 방법
+		 */
+		
+		// 상수 : 변경 불가능
+		// 츠로그래밍에서는 상수표현은 데이터타입 앞에 'final' 키워드가 온다.
+		// 상수를 표현할 땐 변수이름을 대문자로 적어야 합니다. (개발자 약속)
+		final double PI = 3.14;
+		
+		/*
+		 * 문제) 원의 반지름이 주어졌을 때, 원의 면적을 계산하는 프로그램을 작성하세요.
+		 * double radius = 5.0; // 원의 반지름
+		 * 원의 면적은 PI * 반지름 * 반지름 입니다.
+		 */
+		double radius = 5.0;
+		System.out.println(PI * radius * radius);
+		
+		/*
+		 * 문제) 피타고라스
+		 * 직각삼각형의 한 변의 길이를 나타내는 정수 a와 빗변의 길이를 나타내는 정수 c가
+		 * 변수로 주어질 때, 한 변의 길이의 제곱, b를 출력하시오.
+		 */
+		int a = 3;
+		int c = 5;
+		int b = c*c - a*a; // b구하기 => 16이 나와야함
+		System.out.println(b);
+		
+		int[] array = {1,3,5}; // 배열선언
+		System.out.println(array[0]);
+		// 맨 마지막 인덱스 출력하기
+//		System.out.println(array[2]); 세모
+		System.out.println(array[array.length-1]);
+		
+		// 문제 1번 인덱스 값과 2번 인덱스 값 서로 교환하기
+		
+		int temp = array[1]; // temp(임시공간 라는 의미)에 3대입
+		array[1] = array[2]; // 1번 인덱스에 5대입
+		array[2] = temp; // 2번 인덱스에 3대입
+		// swap
+		
+		// 배열문제가 나오면 모조건 for문
+		for(int i : array) { // foreach문(배열 길이만큼 자동으로 반복)
+			System.out.println("i : "+i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
